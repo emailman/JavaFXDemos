c1543882b06b416af067ab37dac04d7f546b2ddb
emailman <emailman@dtcc.edu>
2015-10-21 11:26:51 -0400

    Add demo 5

--- src/p5/JavaFXDemo5.java
+++ src/p5/JavaFXDemo5.java
@@ -1,24 +1,24 @@
-package p4;
+package p5;
 
 /*
  * Created by emailman on 10/21/2015.
  */
 
-// This demo shows a simple pane
+// This demo shows the use of a stack pane
 
 import javafx.application.Application;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class JavaFXDemo4 extends Application {
+public class JavaFXDemo5 extends Application {
 
     @Override
     public void start(Stage primaryStage)  {
 
         // Create a stack pane
-        Pane pane = new Pane();
+        StackPane pane = new StackPane();
 
         // Create a button
         Button bt1 = new Button("This is a big button");
@@ -30,7 +30,7 @@ public class JavaFXDemo4 extends Application {
         Scene scene = new Scene(pane, 250, 50);
 
         // Setup the stage and show it
-        primaryStage.setTitle("Button in a pane");
+        primaryStage.setTitle("Button in a stack pane");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
